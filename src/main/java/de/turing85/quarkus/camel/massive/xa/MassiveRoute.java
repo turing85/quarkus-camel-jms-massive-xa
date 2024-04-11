ded12225544fc15148471fe44770e14c41e60c25
Marco Bungart <marco.bungart@googlemail.com>
2024-04-11 20:16:29 +0200

    content commit for database reproducer

--- src/main/java/de/turing85/quarkus/camel/massive/xa/MassiveRoute.java
+++ src/main/java/de/turing85/quarkus/camel/massive/xa/MassiveRoute.java
@@ -3,52 +3,44 @@ package de.turing85.quarkus.camel.massive.xa;
 import java.time.Duration;
 
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.jms.ConnectionFactory;
 
-import io.smallrye.common.annotation.Identifier;
+import io.agroal.api.AgroalDataSource;
 import org.apache.camel.builder.RouteBuilder;
-import org.springframework.transaction.PlatformTransactionManager;
 
-import static org.apache.camel.builder.endpoint.StaticEndpointBuilders.jms;
 import static org.apache.camel.builder.endpoint.StaticEndpointBuilders.scheduler;
+import static org.apache.camel.builder.endpoint.StaticEndpointBuilders.sql;
 
 @ApplicationScoped
 public class MassiveRoute extends RouteBuilder {
   public static final String PROPERTY_COUNT = "count";
   public static final String PROPERTY_LOOP_COUNT = "loopCount";
 
-  private final ConnectionFactory connectionFactory;
-  private final PlatformTransactionManager globalTransactionManager;
+  private final AgroalDataSource dataSource;
 
   public MassiveRoute(
-      @SuppressWarnings("CdiInjectionPointsInspection") ConnectionFactory connectionFactory,
-      @Identifier(TransactionManagerConfig.GLOBAL_PLATFORM_TRANSACTION_MANAGER_NAME) PlatformTransactionManager globalTransactionManager) {
-    this.connectionFactory = connectionFactory;
-    this.globalTransactionManager = globalTransactionManager;
+      @SuppressWarnings("CdiInjectionPointsInspection") AgroalDataSource dataSource) {
+    this.dataSource = dataSource;
   }
 
   @Override
   public void configure() {
     // @formatter:off
     from(
-        scheduler("send-jms")
+        scheduler("write-to-database")
             .initialDelay(Duration.ofSeconds(5).toMillis())
             .repeatCount(1))
         .transacted()
         .setProperty(PROPERTY_LOOP_COUNT, constant(1000))
-        .log("Starting to send ${exchangeProperty.count} messages")
+        .log("Starting to insert ${exchangeProperty.count} entries")
         .setProperty(PROPERTY_COUNT, constant(0))
         .loop(exchangeProperty(PROPERTY_LOOP_COUNT))
             .process(exchange ->
                 exchange.setProperty(PROPERTY_COUNT, exchange.getProperty(PROPERTY_COUNT, int.class) + 1))
-            .log("Sending message #${exchangeProperty.count}")
-            .setBody(simple("Hello ${exchangeProperty.count}"))
-            .to(jms("send-message")
-                .connectionFactory(connectionFactory)
-                .advanced()
-                    .transactionManager(globalTransactionManager))
+            .log("Inserting entry #${exchangeProperty.count}")
+            .to(sql("INSERT INTO data(id) VALUES(:#${exchangeProperty.count})")
+                .dataSource(dataSource))
         .end()
-        .log("${exchangeProperty.count} messages sent")
+        .log("${exchangeProperty.count} entries inserted")
         .removeProperty(PROPERTY_LOOP_COUNT)
         .removeProperty(PROPERTY_COUNT);
     // @formatter:on
---
+++ src/main/resources/init.sql
@@ -0,0 +1,3 @@
+CREATE TABLE data (
+    id BIGINT PRIMARY KEY
+);
